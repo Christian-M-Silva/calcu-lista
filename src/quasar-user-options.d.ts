fdb6a61ec2a9fa11024ec69634799a75e550f37c
Christian Silva <tininjacms@gmail.com>
2024-09-18 09:42:13 -0300

    feat(addQuasar): Adicionará o Quasar ao projeto

---
+++ src/quasar-user-options.d.ts
@@ -0,0 +1,7 @@
+import { QuasarPluginOptions } from "quasar";
+
+const quasarUserOptions: QuasarPluginOptions = {
+  // Suas opções personalizadas para o Quasar
+};
+
+export default quasarUserOptions;
