35ada7df42ef94e3a7f369070e99f43566b42f84
Christian Silva <tininjacms@gmail.com>
2024-09-25 16:59:33 -0300

    feat(background): Add um background com svg e um estilo de vidro

--- src/pages/home/HomePage.vue
+++ src/pages/home/HomePage.vue
@@ -1,28 +1,45 @@
 <template>
-  <div class="mx-3">
-    <div class="row justify-center">
-      <div class="rounded-xl bg-primary text-white min-h-screen">
-        <header class="bg-black">
-          <h2 class="py-5">Calcu-lista</h2>
-          <div class="row justify-center gap-7">
-            <q-btn color="primary" icon="check" label="OK" @click="onClick" />
-            <input-component
-              v-model="text"
-              type="text"
-              placeholder="Busque por um item"
-              bg-color="white"
-            />
-            <q-btn color="primary" icon="check" label="OK" @click="onClick" />
-          </div>
-        </header>
-        <div class="row">
-          <q-checkbox left-label v-model="orange" />
-          <q-input v-model="text" type="text" label="Qtd" />
-          <q-input v-model="text" type="text" label="Item" />
-          <q-input v-model="text" type="text" label="Valor Unidade" />
-          <q-input v-model="text" type="text" label="Valor total" />
-          <q-btn color="primary" icon="check" label="OK" @click="onClick" />
+  <div class="row justify-center bg-image bg-no-repeat bg-cover bg-center">
+    <div :class="`${$q.screen.lt.md ? '' : 'rounded-xl'} text-white`">
+      <header class="pt-4">
+        <h2 class="py-8">Calcu-lista</h2>
+        <div class="row justify-center gap-7 pb-9">
+          <q-btn
+            text-color="white"
+            icon="vertical_align_bottom"
+            @click="onClick"
+            outline
+            rounded
+          />
+          <input-component
+            v-model="text"
+            type="text"
+            placeholder="Busque por um item"
+            bg-color="white"
+          />
+          <q-btn
+            text-color="white"
+            icon="import_export"
+            @click="onClick"
+            outline
+            rounded
+          />
         </div>
+      </header>
+      <main
+        class="ellipsis glass rounded-t-3xl min-h-[calc(100vh-14.5rem)] pt-5 px-4"
+      >
+        <q-card dark class="bg-grey-9 rounded-borders">
+          <q-card-section class="row">
+            <q-checkbox left-label v-model="orange" />
+            <q-input v-model="text" type="text" label="Qtd" />
+            <q-input v-model="text" type="text" label="Item" />
+            <q-input v-model="text" type="text" label="Valor Unidade" />
+            <q-input v-model="text" type="text" label="Valor total" />
+            <q-btn color="primary" icon="check" label="OK" @click="onClick" />
+          </q-card-section>
+        </q-card>
+
         <div class="row">
           <span>R$ 650,00</span>
           <q-btn
@@ -38,7 +55,7 @@
             @click="onClick"
           />
         </div>
-      </div>
+      </main>
     </div>
   </div>
 </template>
