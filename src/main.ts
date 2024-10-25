6cf96e751f6d9b4d9af4b3bdbeb8a731e17410ad
Christian Silva <tininjacms@gmail.com>
2024-10-25 14:36:28 -0300

    feat(addVmoney): Add o pacote do vmoney

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import { Quasar } from "quasar";
 import quasarUserOptions from "./quasar-user-options";
+import { Money3Directive } from "v-money3";
 
-createApp(App).use(Quasar, quasarUserOptions).use(router).mount("#app");
+createApp(App)
+  .directive("money3", Money3Directive)
+  .use(Quasar, quasarUserOptions)
+  .use(router)
+  .mount("#app");
